2c6923be83423e9d0b91142c0c332a62428f0d84
Kenvix Zure <kenvixzure@live.com>
2020-09-12 11:08:26 +0800

    Manual PunishCommand 3

--- MiraiBot/src/main/java/com/kenvix/complexbot/feature/inspector/PunishCommand.kt
+++ MiraiBot/src/main/java/com/kenvix/complexbot/feature/inspector/PunishCommand.kt
@@ -12,29 +12,33 @@ object PunishCommand : BotCommandFeature {
     override val description: String = "回复某句消息来快速对该消息的发送者执行某个惩罚（仅限管理员）"
 
     override suspend fun onMessage(msg: MessageEvent) {
-        val quotes = msg.message.filterIsInstance<QuoteReply>()
+        try {
+            val quotes = msg.message.filterIsInstance<QuoteReply>()
 
-        if (quotes.isNotEmpty()) {
-            val command = parseCommandFromMessage(msg.message.filterIsInstance<PlainText>().joinToString(" "))
-            val punish = punishments[command.firstArgumentOrNull?.toLowerCase()] ?: Withdraw
+            if (quotes.isNotEmpty()) {
+                val command = parseCommandFromMessage(msg.message.filterIsInstance<PlainText>().joinToString(" "))
+                val punish = punishments[command.firstArgumentOrNull?.toLowerCase()] ?: Withdraw
 
-            quotes.forEach {
-                val target = (msg.subject as Group).members[it.source.fromId]
+                quotes.forEach {
+                    val target = (msg.subject as Group).members[it.source.fromId]
 
-                punish.punish(
-                    msg.subject as Group,
-                    target,
-                    "Manual Operation",
-                    it.source,
-                    ManualPunishmentRule
-                )
+                    punish.punish(
+                        msg.subject as Group,
+                        target,
+                        "Manual Operation",
+                        it.source,
+                        ManualPunishmentRule
+                    )
 
-                if ((msg.sender as Member).permission.level >= 1)
-                    msg.message.recall()
+                    if ((msg.sender as Member).permission.level == 0)
+                        msg.message.recall()
+                }
+            } else {
+                msg.reply("命令用法错误：此命令必须回复一条消息。被回复人为被惩罚者\n" +
+                        "然后，输入要执行的惩罚名称。惩罚名称参见 .inspector help  （默认为 withdraw）")
             }
-        } else {
-            msg.reply("命令用法错误：此命令必须回复一条消息。被回复人为被惩罚者\n" +
-                    "然后，输入要执行的惩罚名称。惩罚名称参见 .inspector help  （默认为 withdraw）")
+        } catch (e: Exception) {
+            msg.reply(e.toString())
         }
     }
 }
